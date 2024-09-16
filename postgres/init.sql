a98e684ca4e3b75094b28ac266a84998d7e43af5
Kenneth Man <kenneth.waikin.man@outlook.com>
2024-09-16 09:51:51 +0100

    fix init.sql

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
 	id serial PRIMARY KEY,
-	username VARCHAR ( 100 ) UNIQUE NOT NULL,
-	password VARCHAR ( 500 ) NOT NULL,
+	username VARCHAR (500) NOT NULL,
+	password VARCHAR (500) NOT NULL,
 	email VARCHAR (500) UNIQUE NOT NULL
 );
 
