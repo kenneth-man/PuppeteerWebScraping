fa8b8a290fed74006fdd643479b75451af493700
Kenneth Man <kenneth.waikin.man@outlook.com>
2024-11-24 13:17:42 +0000

    comment pg to test backend container

--- backend/handlers/auth/signUp.ts
+++ backend/handlers/auth/signUp.ts
@@ -2,7 +2,7 @@ import { Response } from "express"
 import { z } from "zod"
 import { IRequestBody } from "../../models/interfaces"
 import { TPreAccountUser, TSignUp, TUser } from "../../models/types"
-import { client } from "../../server"
+// import { client } from "../../server"
 import { addRow, getUserByEmail, hashPassword, signToken, throwError, tryCatch } from "../../utils"
 import { jwtTokenName } from "../../constants/strings"
 
@@ -12,59 +12,59 @@ const signUp = async (
 ) => {
 	await tryCatch(
 		async () => {
-			if (!req.body?.email || !req.body?.password || !req.body?.username) {
-				throwError(res, 400,
-					"Request must contain a body with the following fields: " + 
-					"email, password, username"
-				)
-			}
+			// if (!req.body?.email || !req.body?.password || !req.body?.username) {
+			// 	throwError(res, 400,
+			// 		"Request must contain a body with the following fields: " + 
+			// 		"email, password, username"
+			// 	)
+			// }
 		
-			const { email, password, username } = req.body
-			const emailLowerCase = email.toLowerCase()
-			const passwordLength = 8
+			// const { email, password, username } = req.body
+			// const emailLowerCase = email.toLowerCase()
+			// const passwordLength = 8
 		
-			if (password.length < passwordLength) {
-				throwError(res, 400,
-					`Passwords must be at least ${passwordLength} characters or greater`
-				)
-			}
+			// if (password.length < passwordLength) {
+			// 	throwError(res, 400,
+			// 		`Passwords must be at least ${passwordLength} characters or greater`
+			// 	)
+			// }
 		
-			const emailValidation = z.string().email().safeParse(emailLowerCase)
+			// const emailValidation = z.string().email().safeParse(emailLowerCase)
 		
-			if (!emailValidation.success) {
-				throwError(res, 400, "Please provide a valid email address")
-			}
+			// if (!emailValidation.success) {
+			// 	throwError(res, 400, "Please provide a valid email address")
+			// }
 		
-			const existingEmail = await client
-				.query(`SELECT * FROM users WHERE email = $1`, [emailLowerCase])
+			// const existingEmail = await client
+			// 	.query(`SELECT * FROM users WHERE email = $1`, [emailLowerCase])
 
-			if (existingEmail.rowCount > 0) {
-				throwError(res, 400,
-					"A user with this email already exists. " + 
-					"Please sign up with a different email address"
-				)
-			}
+			// if (existingEmail.rowCount > 0) {
+			// 	throwError(res, 400,
+			// 		"A user with this email already exists. " + 
+			// 		"Please sign up with a different email address"
+			// 	)
+			// }
 
-			let newUser: TPreAccountUser = {
-				username,
-				email: emailLowerCase
-			}
+			// let newUser: TPreAccountUser = {
+			// 	username,
+			// 	email: emailLowerCase
+			// }
 
-			const passwordHash = hashPassword(password)
-			newUser.password = passwordHash
+			// const passwordHash = hashPassword(password)
+			// newUser.password = passwordHash
 
-			await addRow(client, newUser, "users")
+			// await addRow(client, newUser, "users")
 
-			const user: TUser = await getUserByEmail(res, emailLowerCase)
+			// const user: TUser = await getUserByEmail(res, emailLowerCase)
 
-			delete user.password
+			// delete user.password
 
-			const token = signToken(user)
+			// const token = signToken(user)
 
-			res
-				.status(200)
-				.cookie(jwtTokenName, token, { httpOnly: true })
-				.json({...user});
+			// res
+			// 	.status(200)
+			// 	.cookie(jwtTokenName, token, { httpOnly: true })
+			// 	.json({...user});
 		},
 		res
 	)
