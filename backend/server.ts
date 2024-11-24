8698ae8bf4a581502589201b0192e962d03ac3c2
Kenneth Man <kenneth.waikin.man@outlook.com>
2024-11-24 13:13:53 +0000

    comment pg to test backend container

--- backend/server.ts
+++ backend/server.ts
@@ -1,17 +1,17 @@
-import config from "config"
-import pg from "pg"
+// import config from "config"
+// import pg from "pg"
 import app from "./app"
 import { apiPort, postgresPort } from "./constants/numbers"
 
-export const client = new pg.Client({
-	host: config.get("VARS.postgres_host"),
-	port: postgresPort,
-	database: config.get("VARS.postgres_db_name"),
-	user: config.get("VARS.postgres_user"),
-	password: config.get("VARS.postgres_password")
-})
+// export const client = new pg.Client({
+// 	host: config.get("VARS.postgres_host"),
+// 	port: postgresPort,
+// 	database: config.get("VARS.postgres_db_name"),
+// 	user: config.get("VARS.postgres_user"),
+// 	password: config.get("VARS.postgres_password")
+// })
 
-client.connect();
+// client.connect();
 
 app.listen(
 	apiPort,
@@ -19,7 +19,7 @@ app.listen(
 		console.log("API is running...")
 		console.log(`> Port: ${apiPort}`)
 		try {
-			await client.query("SELECT * FROM users WHERE id = 1")
+			// await client.query("SELECT * FROM users WHERE id = 1")
 			console.log(`> Connected to Postgres Server...`)
 		} catch(e) {
 			console.log("> Could not connect to postgres server...")
